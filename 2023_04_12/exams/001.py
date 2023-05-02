734d9b1b1e56ac0b4ef7ab71d83f7875faf185cd
Alex Carrega <contact@alexcarrega.com>
2023-05-02 16:11:01 +0200

    Pandas + Chart

---
+++ 2023_04_12/exams/001.py
@@ -0,0 +1,249 @@
+
+
+-----------------------
+./Baglietto-Luca - Luca Baglietto/001.py
+if __name__ == '__main__':
+    a = int(input("Inserisci il primo numero: "))
+    b = int(input("Inserisci il secondo numero: "))
+    if a > b:
+        mx = a
+    else:
+        mx = b
+    print("Il massimo è", mx)
+
+
+-----------------------
+./roggero_costantino/001.py
+firstnumber = float(input("Inserisci il primo numero: "))
+secondnumber = float(input("Inserisci il secondo numero: "))
+
+if firstnumber > secondnumber:
+    print("Il numero più grande è:", firstnumber)
+elif firstnumber < secondnumber:
+    print("Il numero più grande è:", secondnumber)
+
+-----------------------
+./lin-matteo - Matth6ew/001.py
+def main():
+    a = float(input('primo numero:'))
+    b = float(input('secondo numero:'))
+    if a >= b:
+        print(a)
+    else:
+        print(b)
+
+main()
+
+
+-----------------------
+./Zanardi-Leonardo.zip - leonardo zanardi/001.py
+def max_2():
+    x = float(input('first number: '))
+    y = float(input('second number: '))
+    
+    if x > y:
+        print(x)
+    else:
+        print(y)
+        
+max_2()
+
+-----------------------
+./ExamFrittoli - Lorenzo Frittoli/001.py
+def max_2():
+    a = float(input('1st number: '))
+    b = float(input('2nd number: '))
+    
+    if a > b:
+        print(a)
+    else:
+        print(b)
+        
+max_2()
+
+-----------------------
+./Alessandro-Lugaro/001.py
+n1 = float(input("Inserisci il primo numero: "))
+n2 = float(input("Inserisci il secondo numero: "))
+
+if n1 > n2:
+    print("Il numero più grande è:", n1)
+elif n1 < n2:
+    print("Il numero più grande è:", n2)
+
+
+-----------------------
+./Esercizi informatica/001.py
+# Chiedi all'utente di inserire due numeri
+numero1 = float(input("Inserisci il primo numero: "))
+numero2 = float(input("Inserisci il secondo numero: "))
+
+# Confronta i due numeri utilizzando le istruzioni if, elif ed else
+if numero1 > numero2:
+    print("Il numero più grande è:", numero1)
+elif numero1 < numero2:
+    print("Il numero più grande è:", numero2)
+else:
+    print("I numeri sono uguali!")
+
+-----------------------
+./botta-alessio - Alessio Botta/001.py
+def max_2():
+    a = float(input('1st number: '))
+    b = float(input('2nd number: '))
+    
+    if a > b:
+        print(a)
+    else:
+        print(b)
+        
+max_2()
+
+-----------------------
+./esercizi informatica minetti giulio - Giulio Minetti/001.py
+def max_2():
+    a = float(input('1st number: '))
+    b = float(input('2nd number: '))
+    
+    if a > b:
+        print(a)
+    else:
+        print(b)
+        
+max_2()
+
+-----------------------
+./Esercizi informatica 3/001.py
+# Chiedi all'utente di inserire due numeri
+numero1 = float(input("Inserisci il primo numero: "))
+numero2 = float(input("Inserisci il secondo numero: "))
+
+# Confronta i due numeri utilizzando le istruzioni if, elif ed else
+if numero1 > numero2:
+    print("Il numero più grande è:", numero1)
+elif numero1 < numero2:
+    print("Il numero più grande è:", numero2)
+else:
+    print("I numeri sono uguali!")
+
+-----------------------
+./todde-lorenzo - lorenzo todde/001.py
+def main(): 
+    num1 = input("Inserisci il primo numero: ")
+    num2 = input("Inserisci il secondo numero: ")
+    if num1 >= num2:
+        print(num1)
+    else :
+        print(num2)
+
+main()
+
+
+-----------------------
+./dapino-berenice/001.py
+def main(): 
+    n1 = float(input("Inserisci il primo numero: "))
+    n2 = float(input("inserisci il secondo numero: "))
+    if n1 > n2: 
+        print("Numero maggiore:", n1)
+    elif n1 == n2:
+        print(n1)
+    else: 
+        print("Numero maggiore:", n2)
+
+
+main()
+     
+
+-----------------------
+./Esercizi informatica 2/001.py
+# Chiedi all'utente di inserire due numeri
+numero1 = float(input("Inserisci il primo numero: "))
+numero2 = float(input("Inserisci il secondo numero: "))
+
+# Confronta i due numeri utilizzando le istruzioni if, elif ed else
+if numero1 > numero2:
+    print("Il numero più grande è:", numero1)
+elif numero1 < numero2:
+    print("Il numero più grande è:", numero2)
+else:
+    print("I numeri sono uguali!")
+
+-----------------------
+./portella-francesco - Francesco Portella/001.py
+def main():
+    a = float(input('primo numero:'))
+    b = float(input('secondo numero:'))
+    if a >= b:
+        print(a)
+    else:
+        print(b)
+
+main()
+
+
+-----------------------
+./zumiani-sara.zip - Sara Zumiani/001.py
+def max_2():
+    a = float(input('1st number: '))
+    b = float(input('2nd number: '))
+    
+    if a > b:
+        print(a)
+    else:
+        print(b)
+        
+max_2()
+
+-----------------------
+./Hunag-Marco.zip - Marco Huang/001.py
+# Chiedi ala persona designata tre numeri
+numero1 = float(input("Inserisci il primo numero: "))
+numero2 = float(input("Inserisci il secondo numero: "))
+numero3 = float(input("Inserisci il terzo numero: "))
+
+# qual'è il più grande?
+if numero1 > numero2 and numero1 > numero3:
+    print("Il numero più grande è:", numero1)
+elif numero2 > numero1 and numero2 > numero3:
+    print("Il numero più grande è:", numero2)
+elif numero3 > numero1 and numero3 > numero2:
+    print("Il numero più grande è:", numero3)
+else:
+    print("I numeri sono uguali!")
+
+-----------------------
+./zono veran - Fabrizio Verani/001.py
+def max_2():
+    a = float(input('1st number: '))
+    b = float(input('2nd number: '))
+    
+    if a > b:
+        print(a)
+    else:
+        print(b)
+        
+max_2()
+
+-----------------------
+./galvani-vezzi-lidia.zip - Lidia Galvani Vezzi/001.py
+def max_2():
+    a = float(input('1st number: '))
+    b = float(input('2nd number: '))
+    
+    if a > b:
+        print(a)
+    else:
+        print(b)
+        
+max_2()
+
+-----------------------
+./Matteo-Marazzi/001.py
+n1 = float(input("Inserisci il primo numero: "))
+n2 = float(input("Inserisci il secondo numero: "))
+
+if n1 > n2:
+    print("Il numero più grande è:", n1)
+elif n1 < n2:
+    print("Il numero più grande è:", n2)
